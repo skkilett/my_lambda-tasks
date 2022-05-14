0cf865189133ab04447dc59844cb3c7c3d55f56d
skkilett <skkilett3@gmail.com>
2022-05-14 20:50:47 +0300

    add_ip

--- Geoposition/controller.js
+++ Geoposition/controller.js
@@ -4,7 +4,7 @@ export default new class GetLocationController{
   async getLocation(req, res){
     // eslint-disable-next-line no-unused-vars
     const ip = GetLocationService.getIp(req,res);
-    const ipNum = GetLocationService.IptoNum('83.229.33.3');
+    const ipNum = GetLocationService.IptoNum(ip);
     const locationObj = await GetLocationService.getCountryAndIp(ipNum);
     return locationObj;
   }
